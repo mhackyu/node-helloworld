0aac9fdd890931338ccba3945e04f5c0bf284b67
Mark Christian <mark.christian.paderes@gmail.com>
2020-05-07 16:06:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send({ msg: 'Hello World!' });
+  res.send({ msg: 'Hello World!', env: process.env.SAMPLE });
 });
 
 app.listen(PORT, () => {
